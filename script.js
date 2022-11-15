042eb970ca8aa565659de8757347a5bb4707a812
Philip Clark <ContactPhilipClark@protonmail.com>
2022-11-15 01:11:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,153 @@
+let marker = 'x';
+let win = '';
+const winLength = 8;
+
+const gameBoard = (() => {
+  const size = 20;
+
+  let rows = [];
+  const buildGrid = () => {
+    for (let i = 0; i < size; i++) {
+      let cells = [];
+      for (let i = 0; i < size; i++) {
+        cells.push('');
+      }
+      rows.push(cells);
+    }
+    return rows;
+  };
+
+  const grid = buildGrid();
+  document.documentElement.style.setProperty('--size', size);
+  return {
+    grid,
+  };
+})();
+
+const renderer = (() => {
+  const renderRow = (row, rowId) => {
+    let cells = [];
+    let cellId = 0;
+
+    row.forEach((cell) => {
+      cells.push(
+        `<li onclick="markCell(${rowId},${cellId})" value="${cell}" id='${rowId}-${cellId}' ></li>`
+      );
+      cellId++;
+    });
+
+    return `<ul>${cells.join('')}</ul>`;
+  };
+
+  const renderGameBoard = (parent, gameBoard) => {
+    const rows = [];
+    let rowId = 0;
+    gameBoard.grid.forEach((element) => {
+      rows.push(renderRow(element, rowId));
+      rowId++;
+    });
+    parent.innerHTML = rows.join('');
+  };
+  return {
+    renderGameBoard,
+  };
+})();
+
+const checker = (() => {
+  let currentLine = 0;
+  const check = (x, y) => {
+    const templateDirections = [
+      { x: 1, y: 0 },
+      { x: 1, y: 1 },
+      { x: 0, y: 1 },
+      { x: -1, y: 1 },
+      { x: -1, y: 0 },
+      { x: -1, y: -1 },
+      { x: 0, y: -1 },
+      { x: 1, y: -1 },
+    ];
+
+    let directions = templateDirections;
+
+    const invterval = setInterval(() => {
+      if (directions.length > 0 && win == '') {
+        const cell = getElementAtNextLocation(x, y, directions);
+        if (cell != undefined) {
+          if (cell.getAttribute('value') == (marker == 'x' ? 'o' : 'x')) {
+            cell.style.border = 'solid #00ff2fff 5px';
+            checkLine(x, y, directions, 1, { x: x, y: y });
+          }
+        }
+        directions.shift();
+      } else {
+        clearInterval(invterval);
+      }
+    }, 0);
+  };
+
+  const checkLine = (x, y, direction, opposite, original) => {
+    const nextCell = getElementAtNextLocation(x, y, direction);
+
+    if (nextCell != undefined) {
+      if (nextCell.getAttribute('value') == (marker == 'x' ? 'o' : 'x')) {
+        nextCell.style.border = 'solid #00ff2fff 5px';
+        currentLine++;
+        if (currentLine >= winLength) {
+          win = `${marker == 'x' ? 'Yellow' : 'Pink'} is the Winner`;
+          document.getElementById('winText').innerText = win; //! Remove later
+
+          console.log(win);
+
+          return;
+        }
+        checkLine(
+          x + direction[0].x * opposite,
+          y + direction[0].y * opposite,
+          direction,
+          opposite,
+          original
+        );
+      }
+    }
+    if (opposite != -1) {
+      opposite = -1;
+      x = original.x;
+      y = original.y;
+      checkLine(
+        x + direction[0].x * opposite,
+        y + direction[0].y * opposite,
+        direction,
+        opposite,
+        original
+      );
+    } else {
+      currentLine = 0;
+    }
+  };
+
+  const getElementAtNextLocation = (x, y, directions) => {
+    return getElementAtCoords(x + directions[0].x, y + directions[0].y);
+  };
+
+  const getElementAtCoords = (x, y) => {
+    return document.getElementById(`${y}-${x}`);
+  };
+
+  return { check };
+})();
+
+renderer.renderGameBoard(document.getElementById('board'), gameBoard);
+
+function markCell(rowId, cellId) {
+  if (gameBoard.grid[rowId][cellId] == '' && win == '') {
+    gameBoard.grid[rowId][cellId] = marker;
+    marker = marker == 'x' ? 'o' : 'x';
+
+    checker.check(cellId, rowId);
+    document.documentElement.style.setProperty(
+      '--hoverColor',
+      marker == 'o' ? '#ffd866' : '#ff6188'
+    );
+    renderer.renderGameBoard(document.getElementById('board'), gameBoard);
+  }
+}
